88c16e1a3b0619bcd7c0a724ac028e01147b63ec
aleksei_astapov <Alexey.Astapoff@yandex.ru>
2023-11-22 10:05:06 +0300

    Token JWT+Djoser

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -12,4 +12,9 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
